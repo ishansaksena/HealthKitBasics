5a627f4c5fa033734100768e3ce23cba7a3541c8
Ishan Saksena <ishansaksena@gmail.com>
2016-04-23 00:11:14 -0700

    Basic characteristics implemented

--- HealthBasics/HealthBasics/FirstViewController.swift
+++ HealthBasics/HealthBasics/FirstViewController.swift
@@ -15,8 +15,8 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Requesting permission to use HealthKit details
         
+        // Requesting permission to use HealthKit details
         let healthStore: HKHealthStore? = {
             if HKHealthStore.isHealthDataAvailable() {
                 return HKHealthStore()
@@ -36,7 +36,7 @@ class FirstViewController: UIViewController {
         
         let dataTypesToRead = NSSet(objects:dateOfBirthCharacteristic!, biologicalSexCharacteristic!, bloodTypeCharacteristic!)
         
-        let dataTypesToShare = NSSet(objects:dateOfBirthCharacteristic!, biologicalSexCharacteristic!, bloodTypeCharacteristic!)
+        let dataTypesToShare = NSSet()
         
         // Closure for healthStore?.requestAuthorizationToShareTypes
         func authorizeHealthKit()
@@ -52,6 +52,86 @@ class FirstViewController: UIViewController {
             })
         }
         authorizeHealthKit()
+        
+        // Getting characteristics
+        // Getting date of birth of user
+        
+        let dateFormatter: NSDateFormatter = {
+            let formatter = NSDateFormatter()
+            formatter.dateStyle = .LongStyle
+            return formatter
+        }()
+        
+        var dateOfBirth: String? {
+            /*if let dateOfBirth = healthStore?.dateOfBirth() {
+                return dateFormatter.stringFromDate(dateOfBirth)
+            }
+            return nil*/
+            do {
+                let birthday = try healthStore?.dateOfBirth()
+                return dateFormatter.stringFromDate(birthday!)
+            } catch let error as ErrorType {
+                print(error)
+            }
+            return nil
+        }
+        
+        // Asking for gender
+        var biologicalSex: String? {
+            do {
+                let biologicalSex = try healthStore?.biologicalSex()
+                switch biologicalSex!.biologicalSex {
+                case .Female:
+                    return "Female"
+                case .Male:
+                    return "Male"
+                case .Other:
+                    return "Other"
+                case .NotSet:
+                    return nil
+                }
+            } catch let error as NSError {
+                print(error.description)
+            } catch let error as ErrorType {
+                print(error)
+            }
+            return nil
+        }
+        
+        // Getting bloodtype
+        var bloodType: String? {
+            do {
+                let bloodType = try healthStore?.bloodType()
+                switch bloodType!.bloodType {
+                case .APositive:
+                    return "A+"
+                case .ANegative:
+                    return "A-"
+                case .BPositive:
+                    return "B+"
+                case .BNegative:
+                    return "B-"
+                case .ABPositive:
+                    return "AB+"
+                case .ABNegative:
+                    return "AB-"
+                case .OPositive:
+                    return "O+"
+                case .ONegative:
+                    return "O-"
+                case .NotSet:
+                    return nil
+                }
+            } catch let error as ErrorType {
+                print(error)
+            }
+            return nil
+        }
+        
+        // Printing results
+        NSLog(dateOfBirth!)
+        NSLog(biologicalSex!)
+        NSLog(bloodType!)
     }
 
     override func didReceiveMemoryWarning() {
